38404fb58c3f84c03d1f88af8603872f67e80ad0
ProfWendi <jollymor@sheridancollege.ca>
2023-11-21 18:17:22 -0500

    new example

---
+++ nodejsapps/expressMvc/mvc/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const homeController = require("./controllers/homeController");
+
+
+const express = require("express"),
+    app = express();
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+
+app.get("/menu/:day", homeController.renderMenu);
+
+/*
+app.get("/menu/:day", (req, res) => {
+    console.log(`day: ${req.params.day}`);
+});
+*/
+app.listen(app.get("port"), () => {
+    console.log(`Server running on http://localhost:${app.get("port")}`);
+});
