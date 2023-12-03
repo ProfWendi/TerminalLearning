70f4079dcb1ebd339f39a55acab63e5a393f9c1b
ProfWendi <jollymor@sheridancollege.ca>
2023-12-03 16:58:53 -0500

    basic db example, with sessions
    same functionality as /basicdb, but this one does it properly with sessions

---
+++ nodejsapps/database/basicdb_sessions/controllers/dbController.js
@@ -0,0 +1,23 @@
+"use strict";
+const mysql = require("mysql");
+
+exports.getCountries = (req, res, next) => {
+    const pool = mysql.createPool({
+        host: "localhost",
+        user: "guest",
+        password: "tunafish",
+        database: "world",
+        connectionLimit: 10
+    });
+    pool.query("SELECT * FROM country;", (qError, results, fields) => {
+        if (qError) {
+            console.log(`query error: ${qError}`);
+        } else {
+            // store results in session instead of rendering
+            req.session.countries = results;
+            // go to next middleware
+            next();
+            
+        }
+    }); 
+}
---
+++ nodejsapps/database/basicdb_sessions/controllers/homeController.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// add/expose a function to render countries.ejs with the 
+// results from our query that were stored in the session
+exports.renderCountries = (req, res) => {
+    res.render("countries", { countries: req.session.countries });
+}
